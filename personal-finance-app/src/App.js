3f86d7d30bb5cf24b139a0d85204c10b448153e9
wgriffey <wgriffey15@gmail.com>
2022-12-22 14:43:33 -0500

    Initial React Project

---
+++ personal-finance-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          PERSONAL FINANCE APP BABYYYYYYY
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
